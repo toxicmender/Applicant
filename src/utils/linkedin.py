717844092e3e0133c50587e4e7aeac35e1d6ec0c
toxicmender <13049649+toxicmender@users.noreply.github.com>
2020-03-16 19:08:51 +0530

    fix: Corrects the cookie reading in restore session to expect json instead of pickle fix: Corrects the cookie storing as encoded json to prevent `json.decoder.JSONDecodeError: Extra data: line 1 column 218 (char 217)` which is the start of next cookie & end of first cookie. This is to prevent json decoding error due to lack of seperator between the cookies. fix: typo in 2 factor Authentication within login function for LinkedIn class chore: Adds a TODO for checking condition(s) for a intermediate page between login & LinkedIn home

--- src/utils/linkedin.py
+++ src/utils/linkedin.py
@@ -9,40 +9,49 @@ class LinkedIn:
         self.browser.get('https://www.linkedin.com/login?fromSignIn=true')
         self.browser.find_element_by_id('username').send_keys(username)
         self.browser.find_element_by_id('password').send_keys(password)
+        # TODO: Detect which one is correct on the page
+        # self.browser.find_element_by_xpath('/html/body/div/main/div/form/div[3]/button').click()
         self.browser.find_element_by_xpath('/html/body/div/main/div/form/div[4]/button').click()
 
         if twoFA:
             self.browser.find_element_by_id('input__phone_verification_pin').send_keys(input('Enter OTP: '))
             self.browser.find_element_by_id('two-step-submit-button').click()
-            browser.find_element_by_xpath('/html/body/div/div[1]/section/div[2]/div/article/footer/div/div/span/button').click()
+            # TODO: Check if it opens everytime or just the first time on a new OS/IP Address pair
+            # self.browser.find_element_by_xpath('/html/body/div/div[1]/section/div[2]/div/article/footer/div/div/span/button').click()
+
+        cookies = {'list': []}
+        for cookie in self.browser.get_cookies():
+            if '.linkedin.com' in cookie['domain']:
+                cookies['list'].append(cookie)
 
         if overwrite:
-            with open(filepath, 'w') as f:
-                for cookie in browser.get_cookies():
-                    if '.linkedin.com' in cookie['domain']:
-                        json.dump(cookie, f)
+            with open(filepath, 'w') as file:
+                json.dump(cookies, file)
         else:
             try:
-                with open(filepath, 'x') as f:
-                    for cookie in browser.get_cookies():
-                        if '.linkedin.com' in cookie['domain']:
-                            json.dump(cookie, f)
+                with open(filepath, 'x') as file:
+                    json.dump(cookies, file)
+
             except FileExistsError as err:
                 print(err)
                 print('session cookie already stored in specified filename')
 
     def restore_session(self, filepath = 'cookies.json'):
+        self.browser.get('https://www.linkedin.com/')
         try:
-            with open(self.path, 'r') as file:
-                cookies = pickle.load(file)
-                for cookie in cookies:
+            with open(filepath, 'r') as file:
+                cookies = json.load(file)
+                for cookie in cookies['list']:
                     if 'expiry' in cookie.keys():
                         del cookie['expiry']
                     self.browser.add_cookie(cookie)
             print('Cookies loaded into browser successfully')
+            # Reload page with the cookies
+            self.browser.get('https://www.linkedin.com/jobs')
 
         except FileNotFoundError as err:
             print(err)
             print('Cookies not added to the browser')
 
-
+    def __del__(self):
+        self.browser.quit()
